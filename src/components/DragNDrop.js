ab65ed97c75e435dbc76bd716fac20d3029e4f96
mrkouhadi <teacherbryan@outlook.com>
2022-04-02 05:40:14 +0800

    editin gthe dragNdrop element

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -3,39 +3,38 @@ import DraggableImg from './DraggableImg';
 import { useDrop } from 'react-dnd'
 import imgs from '../imgDATA';
 
-
 const DragNDrop = () => {
     const [board, setBoard] = useState([]);
 
     let ids = [];
+
     const addImgToBoard = ID =>{
         const newItem = imgs.filter(img => img.id === ID)[0];
-        if(ids.includes(ID)) return;
-        setBoard(board => [
-            ...board,
-            newItem
-        ]);
-        ids = [...ids, ID];
+        if(!ids.includes(ID)){
+            setBoard(board => [
+                ...board,
+                newItem
+            ]);
+           return ids = [...ids, ID];
+        };
     }
-
-    console.log(board);
 
     const [{isOver}, drop] = useDrop(() => ({
         accept:'image',
         drop:item=> addImgToBoard(item.id),
         collect:(monitor) =>({
             isOver:!!monitor.isOver(),
-        })
-      }))
+        }),
+      }));
+
   return (
     <>
-        <div className='imgs'  >
+        <div className='imgs'>
             {
                 imgs.map(img =>(
                     <DraggableImg url={img.url} key={img.id} id={img.id}/>
                 ))
-            }  
-                
+            }
         </div>
         <div className='board' ref={drop}>
                 {
