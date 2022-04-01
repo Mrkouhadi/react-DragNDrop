b7344fa1c65c02064dfeba84b443f73875396cce
mrkouhadi <teacherbryan@outlook.com>
2022-04-02 04:12:35 +0800

    adding useDrop

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DraggableImg from './DraggableImg';
+import { useDrop } from 'react-dnd'
+
+const imgs = [
+    {id:0, url:'https://picsum.photos/id/44/400/300'},
+    {id:1, url:'https://picsum.photos/id/111/400/300'},
+    {id:2, url:'https://picsum.photos/id/122/400/300'},
+    {id:3, url:'https://picsum.photos/id/309/400/300'},
+    {id:4, url:'https://picsum.photos/id/139/400/300'},
+];
 
 const DragNDrop = () => {
-    const imgs = [
-        {id:0, url:'https://picsum.photos/id/44/400/300'},
-        {id:1, url:'https://picsum.photos/id/111/400/300'},
-        {id:2, url:'https://picsum.photos/id/122/400/300'},
-        {id:3, url:'https://picsum.photos/id/309/400/300'},
-    ];
+    const [board, setBoard] = useState([])
 
+    const addImgToBoard = ID =>{
+        const newItem = imgs.filter(img => img.id === ID)[0];
+        setBoard(board => [
+            ...board,
+            newItem
+        ]);
+    }
+    const [{isOver}, drop] = useDrop(() => ({
+        accept:'image',
+        drop:item=> addImgToBoard(item.id),
+        collect:(monitor) =>({
+            isOver:!!monitor.isOver(),
+        })
+      }))
   return (
     <>
-        <div className='imgs'>
+        <div className='imgs'  >
             {
                 imgs.map(img =>(
                     <DraggableImg url={img.url} key={img.id} id={img.id}/>
@@ -19,8 +37,13 @@ const DragNDrop = () => {
             }  
                 
         </div>
-        <div className='board'>
-                
+        <div className='board' ref={drop}>
+                {
+                    board.map(img =>(
+                        <DraggableImg url={img.url} key={img.id} id={img.id}/>
+                    ))
+                }
+                {board.length}
         </div>
     </>
   )
--- src/components/DraggableImg.js
+++ src/components/DraggableImg.js
@@ -4,15 +4,17 @@ import { useDrag } from 'react-dnd'
 const DraggableImg = ({url, id}) => {
     const [{isDragging}, drag] = useDrag(()=>({
         type:'image',
+        item:{  // we can more things into the item object
+            id:id
+        }, 
         collect:(monitor) =>({
             isDragging:!!monitor.isDragging(),
         })
     }));
 
-console.log(isDragging, drag);
 
   return <img className='img' src={url}
-              style={{border:isDragging ? "10px solid pink" : "0px"}}
+              style={{border:isDragging ? "4px solid pink" : "0px"}}
               ref={drag}
          />
 }
